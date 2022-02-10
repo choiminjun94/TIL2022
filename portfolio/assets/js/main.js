f97dca84adcd411420bac2be82c8af395fe57e2c
choiminjun94 <1020alswns@gamil.com>
2022-02-10 23:52:01 +0900

    Header 완성

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -1,6 +1,31 @@
 
-/*===== MENU SHOW Y HIDDEN =====*/
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    // Active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    // Remove Menu moblie
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // SHOW
 
@@ -8,7 +33,7 @@
 // HIDDEN
 
 
-/*===== ACTIVE AND REMOVE MENU =====*/
+
 
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
