2d488595b65566bda1978eb96a5f68914516da1c
choiminjun94 <1020alswns@gmail.com>
2022-02-27 15:13:32 +0900

    Const, let , var

--- JS-study/app.js
+++ JS-study/app.js
@@ -3,11 +3,16 @@
 
 // 하기와 같이 하드 코딩을 할 경우 수정이 매우 귀찮기에  이럴때는 Const 즉 상수를 사용해야 한다. 
 
-const a = 6;
-const myName = "Victor"
+// const a = 6;
+// const myName = "Victor"
+let myName = "Victor"
 
-console.log(a+2);
-console.log(a*2);
-console.log(a/2);
+// console.log(a+2);
+// console.log(a*2);
+// console.log(a/2);
+
+console.log("Myname is " + myName);
+
+myName = "Choi "
 
 console.log("Myname is " + myName);
