6e1a551d364e93705a4d2fd8fc8d8b46a5511378
choiminjun94 <1020alswns@gmail.com>
2022-02-27 12:53:54 +0900

    Js Basic Study2

--- JS-study/app.js
+++ JS-study/app.js
@@ -1,1 +1,13 @@
-alert("test");
+// 콘솔을 입력시 html파일의 콘솔에 뜨게 된다. 
+// console.log("haha");
+
+// 하기와 같이 하드 코딩을 할 경우 수정이 매우 귀찮기에  이럴때는 Const 즉 상수를 사용해야 한다. 
+
+const a = 6;
+const myName = "Victor"
+
+console.log(a+2);
+console.log(a*2);
+console.log(a/2);
+
+console.log("Myname is " + myName);
