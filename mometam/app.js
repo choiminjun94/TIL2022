5b69f019744e9c85b7829a7787b92e4ce663ce25
choiminjun94 <1020alswns@gmail.com>
2022-04-01 08:25:39 +0900

    0401) 화면에 사용자 이름 나오게 하기

--- mometam/app.js
+++ mometam/app.js
@@ -7,25 +7,35 @@
 const loginForm = document.getElementById("login-form");
 
 // input과 button을 끌어오기
-const loginInput = loginForm.querySelector("input"); 
-const loginButton = loginForm.querySelector("button");
-
-const link  = document.querySelector("a");
-
+// const loginInput = loginForm.querySelector("input"); 
+// const loginButton = loginForm.querySelector("button");
+// const link  = document.querySelector("a");
 
 // 또다른 표현 
-// const loginInput1 = loginForm.querySelector("#login-form input");
-// const loginButton2 = loginForm.querySelector("#login-form button");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting"); 
 
-// ()을 더하면 브라우저가 보자마자 자동으로 이 function을 실행 시킨다. .
-// ()안에 있는걸 argument라고 한다. 
+// Hidden Css를 변수 처리
+const hidden_ClassName = "hidden"
+
+// ()을 더하면 브라우저가 보자마자 자동으로 이 function을 실행 시킨다, ()안에 있는걸 argument라고 한다. 
+
 function onLoginSubmit(evnet){
     // preventDefault는 어떤 event의 기본행위든 발생 되지 않게 해준다.
     // 브라우저의 동작을 허용하지 않고 막아버린다.
     evnet.preventDefault();
-    console.log(event);
-    console.log(loginInput.value);
-    
+    // console.log(event);
+    // console.log(loginInput.value);
+
+    // username에 loginInput.value값을 저장
+    const uesrname = loginInput.value;
+    // classList 찾아보기
+    loginForm.classList.add(hidden_ClassName)
+    console.log(uesrname);
+    // 이것만 사용하면 hidden class가 적용 되기에 화면상에 보이지 않는다.
+    greeting.innerText = "Hello " + uesrname;
+    greeting.classList.remove(hidden_ClassName)
 }
 
 function handleLinkClick(event){
@@ -37,5 +47,5 @@ function handleLinkClick(event){
 
 // preventDefault를 사용 시 addEventListener 안에 있는 함수는 직접 실행 하지 않는다.
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick)
- 
+// link.addEventListener("click", handleLinkClick)
+
