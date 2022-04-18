fa7bdd75dd5e02cfe89f40cb0bc7ff68e69a6acf
choiminjun94 <1020alswns@gmail.com>
2022-04-19 08:22:36 +0900

    0419) Todo 항목 삭제 진행

--- firebase-mometam/public/js/todo.js
+++ firebase-mometam/public/js/todo.js
@@ -2,13 +2,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+
+function deleteToDo(event){
+    //console.dir(event.target)를 사용하여 parentElement 찾아보기
+    console.dir(event.target);
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    // li는 span이라는 자식을 가지게 된다.
-    li.appendChild(span)
     // span의 텍스트는 handleToDoSubmit에서 온 newTodo 텍스트가 되는거다.
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌"
+    //무언가 클릳 되었는다는것을 알기 위햐선 EventListener를 사용
+    button.addEventListener("click", deleteToDo);
+
+    // li는 span이라는 자식을 가지게 된다.
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
