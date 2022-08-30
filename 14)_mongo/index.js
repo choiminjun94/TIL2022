359c0ef6bc02a4846a0ed24ae17821c4f1a999cb
Choi minjun <60457431+choiminjun94@users.noreply.github.com>
2022-08-31 07:56:13 +0900

    no message

--- 14)_mongo/index.js
+++ 14)_mongo/index.js
@@ -1,5 +1,15 @@
-const express = require('express'); 
-const app = express();
-const port = 3000;
+const express= require('express')
+const app = express()
+const port = 5000
+const mongoose = require('mongoose')
 
-app.get('/', (req, res)=>res.send('Hello World'));
+mongoose.connect('mongodb+srv://test:0000@cluster0-amulf.mongodb.net/STUDY?retryWrites=true&w=majority', {
+  // useNewUrlParser: true, 
+  // useUnifiedTopology: true, 
+  // useCreateIndex: true, 
+  // useFindAndModify: false,
+}).then(()=>console.log('connect')).catch(err=>console.log(err))
+
+app.get('/', (req, res)=> res.send('hello'))
+
+app.listen(port, ()=>console.log(`express app ${port}!`));
