029c7bc20c3ddfdb312ec48f64826d7f66516d91
choiminjun94 <1020alswns@gmail.com>
2022-08-30 23:37:22 +0900

    0830) 몽고 디비 연결

--- 14)_mongo/index.js
+++ 14)_mongo/index.js
@@ -1,5 +1,12 @@
 const express = require('express'); 
 const app = express();
-const port = 3000;
+const port = 5000;
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://minjun:0000@cluster0.ep8wgk5.mongodb.net/?retryWrites=true&w=majority', {
+    //  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=> console.log('몽고 DB 연결중'))
+.catch(err => console.log(err))
+
 
 app.get('/', (req, res)=>res.send('Hello World'));
+app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
