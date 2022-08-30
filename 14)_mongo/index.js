9e8d417f94b90b5bcb646b834d716e136230e82c
Choi minjun <60457431+choiminjun94@users.noreply.github.com>
2022-08-31 08:26:35 +0900

    0831) 스키마 작성

--- 14)_mongo/index.js
+++ 14)_mongo/index.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const express= require('express')
-const app = express()
-const port = 5000
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://test:0000@cluster0-amulf.mongodb.net/STUDY?retryWrites=true&w=majority', {
-  // useNewUrlParser: true, 
-  // useUnifiedTopology: true, 
-  // useCreateIndex: true, 
-  // useFindAndModify: false,
-}).then(()=>console.log('connect')).catch(err=>console.log(err))
-
-app.get('/', (req, res)=> res.send('hello'))
-
-app.listen(port, ()=>console.log(`express app ${port}!`));
-=======
 const express = require('express'); 
 const app = express();
 const port = 5000;
@@ -27,4 +10,3 @@ mongoose.connect('mongodb+srv://minjun:0000@cluster0.ep8wgk5.mongodb.net/?retryW
 
 app.get('/', (req, res)=>res.send('Hello World'));
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
->>>>>>> 029c7bc20c3ddfdb312ec48f64826d7f66516d91
