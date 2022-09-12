0773ba8415f8c35385580c413663072bff21a2e5
choiminjun94 <1020alswns@gmail.com>
2022-09-12 22:21:42 +0900

    0912) 몽고 DB 연결 성공

--- 15)_Mongo_review/index.js
+++ 15)_Mongo_review/index.js
@@ -2,5 +2,14 @@ const express = require('express');
 const app = express()
 const port = 3030
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Node_mongo:0000@cluster0.x2gk0g9.mongodb.net/?retryWrites=true&w=majority',{
+    //  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=> console.log('몽고 DB 연결중'))
+.catch(err => console.log(err))
+
+
+
 app.get('/', (req,res) => res.send('화면 실험'))
 app.listen(port, () => console.log(`현재 포트번호 ${port} 입니다.`));
