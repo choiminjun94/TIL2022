8eccee38a676d7732fdb77ea70de3cd5a2d5b56d
choiminjun94 <1020alswns@gmail.com>
2022-09-12 22:27:33 +0900

    0912) 스카마 작성 완성

--- 15)_Mongo_review/index.js
+++ 15)_Mongo_review/index.js
@@ -5,11 +5,12 @@ const port = 3030
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://Node_mongo:0000@cluster0.x2gk0g9.mongodb.net/?retryWrites=true&w=majority',{
-    //  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(()=> console.log('몽고 DB 연결중'))
+
+}).then(()=> console.log('몽고 DB 연결 성공 했습니다.'))
 .catch(err => console.log(err))
 
 
 
 app.get('/', (req,res) => res.send('화면 실험'))
 app.listen(port, () => console.log(`현재 포트번호 ${port} 입니다.`));
+
